15491d6c6f9a1d70ab59981f0846f5b5d4180801
Omar Cabarcas <cahocyjei@gmail.com>
2022-12-29 12:38:59 -0500

    Preparando deploy a Railway

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const { logErrors,errorHandler,errorBoomHandler} = require('./midlewars/error.handler')
-const validatorHandler = require('./midlewars/validatorHandler');
+const cors = require('cors');
 const express = require('express');
 const routerApi = require('./routes');
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 app.listen(port);
-
-//Midlewars --------------------------
 app.use(express.json());
+//Midlewars --------------------------
+const whitelist = ["http://localhost:8080","https://myapp.co"]
+const options = {
+    origin:(origin,callback)=>{
+        if (whitelist.includes(origin||!origin)) {
+            callback(null,true);
+        }else{
+            callback(new Error('No permitido')) ;
+        }
+    }
+}
 
+app.use(cors(options));
+app.get('/',(req,res)=>{
+    res.send(`${req.url}: Bienvenido a mi servidor con express`);
+})
 routerApi(app);
 app.use(logErrors);
 app.use(errorBoomHandler);
 app.use(errorHandler);
-
-console.log('Escuchando en el puerto:' + port);
